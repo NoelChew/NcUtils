7df83a9388513819dff22515af6093e6c47099dc
NoelChew <chewwengchuen@gmail.com>
2016-08-17 16:47:11 +0800

    Removed Redundant Classes
    - removed Mandrill and Email Util

--- library/src/main/java/com/noelchew/ncutils/ResourceUtil.java
+++ library/src/main/java/com/noelchew/ncutils/ResourceUtil.java
@@ -8,7 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 
 /**
- * Created by NoelChew on 3/14/16.
+ * Created by noelchew on 3/14/16.
  */
 public class ResourceUtil {
     public static Bitmap drawableResourceToBitmap(Context context, int drawableResource) {
--- library/src/main/java/com/noelchew/ncutils/email/EmailListener.java
+++
@@ -1,9 +0,0 @@
-package com.noelchew.ncutils.email;
-
-/**
- * Created by noelchew on 7/22/16.
- */
-public interface EmailListener {
-    void onSuccess();
-    void onError(String errorMessage);
-}
