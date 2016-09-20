5ddb10b97685b79a6c4aad9b68e318c4f1457a36
NoelChew <chewwengchuen@gmail.com>
2016-09-20 16:35:14 +0800

    CryptoUtil
    - added CryptoUtil
    - only MD5 hash function at the moment

---
+++ library/src/main/java/com/noelchew/ncutils/CryptoUtil.java
@@ -0,0 +1,36 @@
+package com.noelchew.ncutils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by noelchew on 20/09/2016.
+ */
+public class CryptoUtil {
+
+    public static String getMd5Hash(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
