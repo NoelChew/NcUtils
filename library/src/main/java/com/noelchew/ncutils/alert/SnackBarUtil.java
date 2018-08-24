2ac5a599c04a6a5671a2ea980ce1d9ed88aa05bc
noelchew <chewwengchuen@gmail.com>
2018-08-24 16:23:56 +0800

    Expose Static Map dependency
    - replace "implementation" with "api"
    - removed support design library dependency (SnackBarUtil removed)

--- library/src/main/java/com/noelchew/ncutils/alert/SnackBarUtil.java
+++ library/src/main/java/com/noelchew/ncutils/alert/SnackBarUtil.java
@@ -1,27 +1,24 @@
 package com.noelchew.ncutils.alert;
 
-import android.support.design.widget.Snackbar;
-import android.view.View;
-
 /**
  * Created by noelchew on 09/12/2016.
  */
 public class SnackBarUtil {
 
-    public static void showShortMessage(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
-    }
-
-    public static void showShortMessage(View view, int messageResource) {
-        Snackbar.make(view, messageResource, Snackbar.LENGTH_SHORT).show();
-    }
-
-    public static void showLongMessage(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
-    }
-
-    public static void showLongMessage(View view, int messageResource) {
-        Snackbar.make(view, messageResource, Snackbar.LENGTH_LONG).show();
-    }
+//    public static void showShortMessage(View view, String message) {
+//        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+//    }
+//
+//    public static void showShortMessage(View view, int messageResource) {
+//        Snackbar.make(view, messageResource, Snackbar.LENGTH_SHORT).show();
+//    }
+//
+//    public static void showLongMessage(View view, String message) {
+//        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+//    }
+//
+//    public static void showLongMessage(View view, int messageResource) {
+//        Snackbar.make(view, messageResource, Snackbar.LENGTH_LONG).show();
+//    }
 
 }
